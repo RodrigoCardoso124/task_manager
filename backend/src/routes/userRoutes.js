38012e1e163cf339909277de03b23c15a594f1b0
Rodrigo-Cardoso54 <rodrigo.cardoso@sisgarbe.pt>
2026-06-15 11:59:38 +0100

    feat: implementa login e geração de token JWT

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const router = express.Router();
 const userController = require('../controllers/userController');
 
-const router = express.Router();
-
-// Rota para o registo: POST http://localhost:5000/api/users/register
+// Rota para registar um novo utilizador
 router.post('/register', userController.register);
+
+// ROTA NOVA: Rota para fazer o login
+router.post('/login', userController.login);
 
 module.exports = router;
